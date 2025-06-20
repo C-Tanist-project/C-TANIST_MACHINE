0312a45d3c11d5373c2648957a555ef55e3d2b78
Vitor <vitor.a.o.4444@gmail.com>
2025-06-20 15:34:50 -0300

    Algumas operacoes da vm

--- src/vm.cpp
+++ src/vm.cpp
@@ -4,61 +4,61 @@ std::map<Opcode, Operations::OpFunc> Operations::execute;
 
 OperandFormat DecodeOperandFormat(int16_t instruction,
                                   unsigned char operandIdx) {
-    Opcode opcode = (Opcode)(instruction & 31);
+  Opcode opcode = (Opcode)(instruction & 31);
 
-    bool indirectSwitch = instruction & (1 << (5 + operandIdx));
-    OperandFormat operandFormat = indirectSwitch ? INDIRECT : DIRECT;
+  bool indirectSwitch = instruction & (1 << (5 + operandIdx));
+  OperandFormat operandFormat = indirectSwitch ? INDIRECT : DIRECT;
 
-    bool immediateSwitch = instruction & (1 << 7);
-    if (immediateSwitch) return operandFormat;
+  bool immediateSwitch = instruction & (1 << 7);
+  if (immediateSwitch) return operandFormat;
 
-    switch (opcode) {
-        case OP_COPY:
-            if (operandIdx == 0) return operandFormat;
-        case OP_ADD:
-        case OP_DIVIDE:
-        case OP_LOAD:
-        case OP_MULT:
-        case OP_SUB:
-        case OP_WRITE:
-            return IMMEDIATE;
-        default:
-            return operandFormat;
-    }
+  switch (opcode) {
+    case OP_COPY:
+      if (operandIdx == 0) return operandFormat;
+    case OP_ADD:
+    case OP_DIVIDE:
+    case OP_LOAD:
+    case OP_MULT:
+    case OP_SUB:
+    case OP_WRITE:
+      return IMMEDIATE;
+    default:
+      return operandFormat;
+  }
 }
 
 int16_t FetchValue(int16_t instruction, unsigned char operandIdx, VMState *vm) {
-    OperandFormat operandFormat = DecodeOperandFormat(instruction, operandIdx);
+  OperandFormat operandFormat = DecodeOperandFormat(instruction, operandIdx);
 
-    switch (operandFormat) {
-        case IMMEDIATE:
-            return vm->memory[vm->pc + operandIdx + 1];
-        case INDIRECT:
-            return vm->memory[vm->memory[vm->pc + operandIdx + 1]];
-        case DIRECT:
-            return vm->memory[vm->pc + operandIdx + 1];
-    }
+  switch (operandFormat) {
+    case IMMEDIATE:
+      return vm->memory[vm->pc + operandIdx + 1];
+    case INDIRECT:
+      return vm->memory[vm->memory[vm->pc + operandIdx + 1]];
+    case DIRECT:
+      return vm->memory[vm->pc + operandIdx + 1];
+  }
 }
 
 void ExecuteStep(VMState *vm) {
-    unsigned char offset = 0;
-    int16_t instruction = vm->memory[vm->pc];
+  unsigned char offset = 0;
+  int16_t instruction = vm->memory[vm->pc];
 
-    Opcode opcode = (Opcode)((instruction & 31) % 19);
+  Opcode opcode = (Opcode)((instruction & 31) % 19);
 
-    switch (opcode) {
-        case OP_COPY:
-            offset = 3;
-            break;
-        case OP_RET:
-        case OP_STOP:
-            offset = 1;
-            break;
-        default:
-            offset = 2;
-    }
+  switch (opcode) {
+    case OP_COPY:
+      offset = 3;
+      break;
+    case OP_RET:
+    case OP_STOP:
+      offset = 1;
+      break;
+    default:
+      offset = 2;
+  }
 
-    Operations::execute[opcode](vm);
+  Operations::execute[opcode](vm);
 
-    vm->pc += offset;
+  vm->pc += offset;
 }
--- src/vm.hpp
+++ src/vm.hpp
@@ -13,65 +13,126 @@
 #include <variant>
 
 typedef enum {
-    IMMEDIATE,
-    DIRECT,
-    INDIRECT
+  IMMEDIATE,
+  DIRECT,
+  INDIRECT
 
 } OperandFormat;
 
 typedef enum {
-    OP_BR = 0,
-    OP_BROPOS = 1,
-    OP_ADD = 2,
-    OP_LOAD = 3,
-    OP_BRZERO = 4,
-    OP_BRNEG = 5,
-    OP_SUB = 6,
-    OP_STORE = 7,
-    OP_WRITE = 8,
-    // OP_UNHA = 9,
-    OP_DIVIDE = 10,
-    OP_STOP = 11,
-    OP_READ = 12,
-    OP_COPY = 13,
-    OP_MULT = 14,
-    OP_CALL = 15,
-    OP_RET = 16,
-    OP_PUSH = 17,
-    OP_POP = 18,
+  OP_BR = 0,
+  OP_BROPOS = 1,
+  OP_ADD = 2,
+  OP_LOAD = 3,
+  OP_BRZERO = 4,
+  OP_BRNEG = 5,
+  OP_SUB = 6,
+  OP_STORE = 7,
+  OP_WRITE = 8,
+  // OP_UNHA = 9,
+  OP_DIVIDE = 10,
+  OP_STOP = 11,
+  OP_READ = 12,
+  OP_COPY = 13,
+  OP_MULT = 14,
+  OP_CALL = 15,
+  OP_RET = 16,
+  OP_PUSH = 17,
+  OP_POP = 18,
 } Opcode;
 
 typedef struct vmstate {
-    int16_t memory[500];
+  int16_t memory[500];
 
-    int16_t pc, sp, acc, mop, ri, re, r0, r1;
+  int16_t pc, sp, acc, mop, ri, re, r0, r1;
 
-    std::shared_mutex mutex;
+  std::shared_mutex mutex;
 
-    std::atomic<bool> sigRun{false}, sigStep{false}, sigPause{false},
-        sigStop{false}, isHalted{false}, hasError{false};
+  std::atomic<bool> sigRun{false}, sigStep{false}, sigPause{false},
+      sigStop{false}, isHalted{false}, hasError{false};
 
 } VMState;
 
 class Operations {
-   public:
-    // um map de Opcodes direto em ponteiros de funções :O
-    // MAIN CHAMA INITIALIZEMAP pra construir o map estático
-    // no final isso aqui é só um hashmap elegante
-    using OpFunc = void (*)(VMState *);
+ public:
+  // um map de Opcodes direto em ponteiros de funções :O
+  // MAIN CHAMA INITIALIZEMAP pra construir o map estático
+  // no final isso aqui é só um hashmap elegante
+  using OpFunc = void (*)(VMState *);
 
-    static std::map<Opcode, OpFunc> execute;
+  static std::map<Opcode, OpFunc> execute;
 
-    static void InitializeMap() {
-        execute[OP_ADD] = &ADD;
-        execute[OP_SUB] = &SUB;
-        execute[OP_MULT] = &MULT;
-        execute[OP_DIVIDE] = &DIVIDE;
+  static void InitializeMap() {
+    execute[OP_BR] = &BR;
+    execute[OP_BROPOS] = &BROPOS;
+    execute[OP_ADD] = &ADD;
+    execute[OP_SUB] = &SUB;
+    execute[OP_MULT] = &MULT;
+    execute[OP_DIVIDE] = &DIVIDE;
+    execute[OP_LOAD] = &LOAD;
+    execute[OP_STORE] = &STORE;
+    execute[OP_WRITE] = &WRITE;
+    execute[OP_BRZERO] = &BRZERO;
+    execute[OP_BRNEG] = &BRNEG;
+    execute[OP_STOP] = &STOP;
+    execute[OP_READ] = &READ;
+    execute[OP_COPY] = &COPY;
+    execute[OP_CALL] = &CALL;
+    execute[OP_RET] = &RET;
+    execute[OP_PUSH] = &PUSH;
+    execute[OP_POP] = &POP;
+  }
+
+  static void BR(VMState *vm) {
+    int16_t operand = FetchValue(vm->memory[vm->pc], 0, vm);
+    vm->pc = operand;
+  }
+  static void BROPOS(VMState *vm) {
+    int16_t operand = FetchValue(vm->memory[vm->pc], 0, vm);
+    if (vm->acc > 0) {
+      vm->pc = operand;
+    } else {
+      vm->pc += 2;
     }
-
-    static void ADD(VMState *vm) {}
-    static void SUB(VMState *vm) {}
-    static void MULT(VMState *vm) {}
-    static void DIVIDE(VMState *vm) {}
+  }
+  static void ADD(VMState *vm) {}
+  static void SUB(VMState *vm) {
+    int16_t operand = FetchValue(vm->memory[vm->pc], 0, vm);
+    vm->acc -= vm->memory[operand];
+  }
+  static void MULT(VMState *vm) {}
+  static void DIVIDE(VMState *vm) {}
+  static void LOAD(VMState *vm) {
+    int16_t operand = FetchValue(vm->memory[vm->pc], 0, vm);
+    vm->acc = vm->memory[operand];
+  }
+  static void STORE(VMState *vm) { vm->memory[vm->pc] = vm->acc; }
+  static void WRITE(VMState *vm) {
+    int16_t operand = FetchValue(vm->memory[vm->pc], 0, vm);
+    std::cout << "Output: " << operand << std::endl;
+  }
+  static void BRZERO(VMState *vm) {
+    int16_t operand = FetchValue(vm->memory[vm->pc], 0, vm);
+    if (vm->acc == 0) {
+      vm->pc = operand;
+    } else {
+      vm->pc += 2;
+    }
+  }
+  static void BRNEG(VMState *vm) {
+    int16_t operand = FetchValue(vm->memory[vm->pc], 0, vm);
+    if (vm->acc < 0) {
+      vm->pc = operand;
+    } else {
+      vm->pc += 2;
+    }
+  }
+  static void STOP(VMState *vm) {}
+  static void READ(VMState *vm) {}
+  static void COPY(VMState *vm) {}
+  static void CALL(VMState *vm) {}
+  static void RET(VMState *vm) {}
+  static void PUSH(VMState *vm) {}
+  static void POP(VMState *vm) {}
 };
 #endif  // !H_VM
