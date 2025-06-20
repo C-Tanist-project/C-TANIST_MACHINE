4365a7e5c78307f803c6350d7a5dec597b3d0104
Eduarda Medeiros <165853048+duhdah@users.noreply.github.com>
2025-06-20 13:15:25 -0300

    Update memory_editor.cpp

--- src/windows/memory_editor.cpp
+++ src/windows/memory_editor.cpp
@@ -7,6 +7,7 @@ void RenderMemoryEditor() {
     static MemoryEditor mem_edit;
     mem_edit.PreviewDataType = ImGuiDataType_U16;
 
+     // Buffer: Vetor sincronizado com as alterações do usuário na interface
     static uint16_t buffer[500], data[500];
     size_t data_size = sizeof(data);
     size_t buffer_size = sizeof(buffer);
@@ -25,6 +26,20 @@ void RenderMemoryEditor() {
         ImGui::Begin("Memory Editor", &mem_edit.Open);
         mem_edit.DrawContents(buffer, buffer_size);
 
+         // Para testar se buffer e data estão funcionando como previsto:
+        /*
+        ImGui::SetCursorPos(
+            ImVec2(ImGui::GetCursorPosX() + ImGui::GetContentRegionAvail().x - 400,
+                   ImGui::GetCursorPosY() + ImGui::GetContentRegionAvail().y - 20));
+        ImGui::Text("buffer[2] = %d", buffer[2]);
+        ImGui::SetCursorPos(
+            ImVec2(ImGui::GetCursorPosX() + ImGui::GetContentRegionAvail().x - 250,
+                   ImGui::GetCursorPosY() + ImGui::GetContentRegionAvail().y - 20));
+        ImGui::Text("data[2] = %d", vm_state.memory[2]);
+        */
+
+        // Botão SAVE
+        // Salva no vetor da VM as modifiações feitas no buffer
         ImGui::Spacing();
         ImGui::NewLine();
         ImVec2 button_size = ImVec2(50, 30);
@@ -37,21 +52,19 @@ void RenderMemoryEditor() {
             memcpy(data, buffer, data_size);
         }
 
+         // Botão LOAD
+        // Permite escolher um arquivo e carregar os dados em buffer
         button_size = ImVec2(50, 30);
         available = ImGui::GetContentRegionAvail();
         ImGui::SetCursorPosX(ImGui::GetCursorPosX() + available.x -
                              button_size.x - 70);
         ImGui::SetCursorPosY(ImGui::GetCursorPosY() + available.y -
                              button_size.y);
-
+    1
         static bool openDialog = false;
-
-        // Botão LOAD
         if (ImGui::Button("Load", button_size)) {
             openDialog = true;
         }
-
-        // Janela do diálogo de arquivos
         if (openDialog) {
             IGFD::FileDialogConfig config;
             config.path = ".";
@@ -59,9 +72,7 @@ void RenderMemoryEditor() {
                 "TxtMemoryDialog", "Escolha o arquivo de memória (.txt)",
                 ".txt", config);
         }
-
         static std::string currentPath;
-
         if (ImGuiFileDialog::Instance()->Display("TxtMemoryDialog")) {
             if (ImGuiFileDialog::Instance()->IsOk()) {
                 std::string filePathName;
