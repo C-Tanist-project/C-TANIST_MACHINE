6991b29ab1864202a80eca932711b8c8a13dd5bc
AlaudeSemCorda <antoniobrum10@gmail.com>
2025-06-20 10:32:06 -0300

    reading binary file

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,40 @@
+#include <fstream>
+#include <iostream>
+
 #include "ui.hpp"
+#include "vm.hpp"
 
-int main() {
-    GLFWwindow *window = MainWindowSetup(1280, 720, "Pentacle VM");
+int main(int argc, char* argv[]) {
+  std::ifstream file(argv[1], std::ios::binary);
+  if (!file) {
+    std::cerr << "Error opening file!\n";
+    return 1;
+  }
 
-    IMGUIsetup(window);
+  VMState* vm = new VMState;
+  int16_t instructionsMemoryAddress = vm->pc;
 
-    while (!glfwWindowShouldClose(window)) {
-        RenderMainWindow(window);
-    }
+  unsigned char buffer[2];
 
-    WindowCleanup(window);
+  while (file.read(reinterpret_cast<char*>(buffer), sizeof(buffer))) {
+    u_int16_t word = (buffer[0] << 8) | (buffer[1]);
+    vm->memory[instructionsMemoryAddress] = word;
+    ++instructionsMemoryAddress;
+  }
 
-    return 0;
+  for (int i = 31; i < 50; ++i) {
+    std::cout << vm->memory[i] << " ";
+  }
+
+  GLFWwindow* window = MainWindowSetup(1280, 720, "Pentacle VM");
+
+  IMGUIsetup(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    RenderMainWindow(window);
+  }
+
+  WindowCleanup(window);
+
+  return 0;
 }
