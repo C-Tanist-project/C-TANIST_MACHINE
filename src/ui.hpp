18df3aa8b35406cad0ae1c7517c317f4ee33044f
matheus renan <99834855+mthsrnn@users.noreply.github.com>
2025-06-20 17:42:19 -0300

    Outro file picker (#5)
    * outro file picker
    
    * desliga o dirent no windows
    
    * desliga o dirent no windows 2
    
    * desliga o dirent no windows 3
    
    * Update memory_editor.cpp
    
    ---------
    
    Co-authored-by: Eduarda Medeiros <165853048+duhdah@users.noreply.github.com>

--- src/ui.hpp
+++ src/ui.hpp
@@ -1,27 +1,34 @@
-#ifndef H_UI
-#define H_UI
-
-#include <GLFW/glfw3.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <iostream>
-
-#include "L2DFileDialog.h"
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include "imgui_memory_editor.h"
-#include "vm.hpp"
-
-void GLFWErrorCallback(int error, const char *description);
-GLFWwindow *MainWindowSetup(const int width, const int height,
-                            const char *title);
-void IMGUIsetup(GLFWwindow *window);
-void RenderMemoryEditor(VMState &vm_state);
-void RenderMainWindow(GLFWwindow *window, VMState &vm_state);
-void WindowCleanup(GLFWwindow *window);
-void RenderControlsWindow(bool &window, VMState &vm_state);
-void SkipToEnd();
-
-#endif  // !H_UI
+﻿#ifndef H_UI﻿
+﻿#define H_UI﻿
+﻿#define USE_STD_FILESYSTEM﻿
+﻿﻿
+﻿#include <GLFW/glfw3.h>﻿
+﻿#include <stdio.h>﻿
+﻿#include <stdlib.h>﻿
+﻿#include <string.h>﻿
+﻿﻿
+﻿#include <algorithm>﻿
+﻿#include <filesystem>﻿
+﻿#include <vector>﻿
+﻿﻿
+﻿#include "external/ImGuiFileDialog.h"﻿
+﻿#include "external/ImGuiFileDialogConfig.h"﻿
+﻿#include "external/imgui_memory_editor.h"﻿
+﻿#include "imgui.h"﻿
+﻿#include "imgui_impl_glfw.h"﻿
+﻿#include "imgui_impl_opengl3.h"﻿
+﻿﻿
+﻿#include "vm.hpp"﻿
+﻿﻿
+﻿void GLFWErrorCallback(int error, const char *description);﻿
+﻿GLFWwindow *MainWindowSetup(const int width, const int height,﻿
+﻿                            const char *title);﻿
+﻿void IMGUIsetup(GLFWwindow *window);﻿
+﻿void RenderMemoryEditor(VMState &vm_state);﻿
+﻿void RenderMainWindow(GLFWwindow *window, VMState &vm_state);﻿
+﻿void WindowCleanup(GLFWwindow *window);﻿
+﻿void RenderControlsWindow(bool &window, VMState &vm_state);﻿
+﻿void SkipToEnd();﻿
+﻿﻿
+﻿#endif  // !H_UI﻿
+﻿
--- src/windows/memory_editor.cpp
+++ src/windows/memory_editor.cpp
@@ -1,90 +1,111 @@
-#include <string>
-
-#include "src/ui.hpp"
-
-void RenderMemoryEditor(VMState& vm_state) {
-  static MemoryEditor mem_edit;
-  mem_edit.PreviewDataType = ImGuiDataType_U16;
-
-  // Buffer: Vetor sincronizado com as alterações do usuário na interface
-  static uint16_t buffer[500];
-  size_t data_size = sizeof(vm_state.memory);
-  size_t buffer_size = sizeof(buffer);
-
-  static bool initialized = false;
-  if (!initialized) {
-    for (int i = 0; i < 500; ++i) buffer[i] = (uint16_t)0;
-    memcpy(vm_state.memory, buffer, data_size);
-    initialized = true;
-  }
-
-  mem_edit.OptShowOptions = false;
-  mem_edit.OptShowDataPreview = true;
-
-  if (mem_edit.Open) {
-    ImGui::Begin("Memory Editor", &mem_edit.Open);
-    mem_edit.DrawContents(buffer, buffer_size);
-
-    // Para testar se buffer e data estão funcionando como previsto:
-    /*
-    ImGui::SetCursorPos(
-        ImVec2(ImGui::GetCursorPosX() + ImGui::GetContentRegionAvail().x - 400,
-               ImGui::GetCursorPosY() + ImGui::GetContentRegionAvail().y - 20));
-    ImGui::Text("buffer[2] = %d", buffer[2]);
-    ImGui::SetCursorPos(
-        ImVec2(ImGui::GetCursorPosX() + ImGui::GetContentRegionAvail().x - 250,
-               ImGui::GetCursorPosY() + ImGui::GetContentRegionAvail().y - 20));
-    ImGui::Text("data[2] = %d", vm_state.memory[2]);
-    */
-
-    // Botão SAVE
-    // Salva no vetor da VM as modifiações feitas no buffer
-    ImGui::Spacing();
-    ImGui::NewLine();
-    ImVec2 button_size = ImVec2(50, 30);
-    ImVec2 available = ImGui::GetContentRegionAvail();
-    ImGui::SetCursorPosX(ImGui::GetCursorPosX() + available.x - button_size.x);
-    ImGui::SetCursorPosY(ImGui::GetCursorPosY() + available.y - button_size.y);
-    if (ImGui::Button("Save", button_size)) {
-      memcpy(vm_state.memory, buffer, data_size);
-    }
-
-    // Botão LOAD
-    // Permite escolher um arquivo e carregar os dados em buffer
-    button_size = ImVec2(50, 30);
-    available = ImGui::GetContentRegionAvail();
-    ImGui::SetCursorPosX(ImGui::GetCursorPosX() + available.x - button_size.x -
-                         70);
-    ImGui::SetCursorPosY(ImGui::GetCursorPosY() + available.y - button_size.y);
-    static bool openDialog = false;
-    static char selectedPath[512] = "";
-    if (ImGui::Button("Load", button_size)) {
-      openDialog = true;
-    }
-    if (openDialog) {
-      FileDialog::ShowFileDialog_s(&openDialog, selectedPath);
-    }
-    if (strlen(selectedPath) > 0) {
-      openDialog = false;
-      ImGui::Text("Arquivo selecionado:");
-      ImGui::Text("%s", selectedPath);
-
-      FILE* f = fopen(selectedPath, "r");
-      if (f) {
-        int16_t value;
-        int i = 0;
-        while (fscanf(f, "%hd", &value) == 1) {
-          buffer[i] = (int16_t)value;
-          i++;
-        }
-        fclose(f);
-        selectedPath[0] = '\0';
-      } else {
-        std::cout << "Erro ao abrir o arquivo: " << selectedPath << "\n";
-        perror("Erro ao abrir o arquivo.");
-      }
-    }
-
-    ImGui::End();
-  }
-}
+﻿﻿
+﻿#include <string>﻿
+﻿﻿
+﻿#include "src/ui.hpp"﻿
+﻿﻿
+﻿void RenderMemoryEditor(VMState& vm_state) {﻿
+﻿    static MemoryEditor mem_edit;﻿
+﻿    mem_edit.PreviewDataType = ImGuiDataType_U16;﻿
+﻿﻿
+﻿     // Buffer: Vetor sincronizado com as alterações do usuário na interface﻿
+﻿  static uint16_t buffer[500];﻿
+﻿  size_t data_size = sizeof(vm_state->memory);﻿
+﻿    size_t buffer_size = sizeof(buffer);﻿
+﻿﻿
+﻿    static bool initialized = false;﻿
+﻿    if (!initialized) {﻿
+﻿        for (int i = 0; i < 500; ++i) buffer[i] = (uint16_t)0;﻿
+﻿    memcpy(vm_state.memory, buffer, data_size);﻿
+﻿        initialized = true;﻿
+﻿    }﻿
+﻿﻿
+﻿    mem_edit.OptShowOptions = false;﻿
+﻿    mem_edit.OptShowDataPreview = true;﻿
+﻿﻿
+﻿    if (mem_edit.Open) {﻿
+﻿        ImGui::Begin("Memory Editor", &mem_edit.Open);﻿
+﻿        mem_edit.DrawContents(buffer, buffer_size);﻿
+﻿﻿
+﻿         // Para testar se buffer e data estão funcionando como previsto:﻿
+﻿        /*﻿
+﻿        ImGui::SetCursorPos(﻿
+﻿            ImVec2(ImGui::GetCursorPosX() + ImGui::GetContentRegionAvail().x - 400,﻿
+﻿                   ImGui::GetCursorPosY() + ImGui::GetContentRegionAvail().y - 20));﻿
+﻿        ImGui::Text("buffer[2] = %d", buffer[2]);﻿
+﻿        ImGui::SetCursorPos(﻿
+﻿            ImVec2(ImGui::GetCursorPosX() + ImGui::GetContentRegionAvail().x - 250,﻿
+﻿                   ImGui::GetCursorPosY() + ImGui::GetContentRegionAvail().y - 20));﻿
+﻿        ImGui::Text("data[2] = %d", vm_state.memory[2]);﻿
+﻿        */﻿
+﻿﻿
+﻿        // Botão SAVE﻿
+﻿        // Salva no vetor da VM as modifiações feitas no buffer﻿
+﻿        ImGui::Spacing();﻿
+﻿        ImGui::NewLine();﻿
+﻿        ImVec2 button_size = ImVec2(50, 30);﻿
+﻿        ImVec2 available = ImGui::GetContentRegionAvail();﻿
+﻿        ImGui::SetCursorPosX(ImGui::GetCursorPosX() + available.x -﻿
+﻿                             button_size.x);﻿
+﻿        ImGui::SetCursorPosY(ImGui::GetCursorPosY() + available.y -﻿
+﻿                             button_size.y);﻿
+﻿        if (ImGui::Button("Save", button_size)) {﻿
+﻿      memcpy(vm_state.memory, buffer, data_size);﻿
+﻿        }﻿
+﻿﻿
+﻿         // Botão LOAD﻿
+﻿        // Permite escolher um arquivo e carregar os dados em buffer﻿
+﻿        button_size = ImVec2(50, 30);﻿
+﻿        available = ImGui::GetContentRegionAvail();﻿
+﻿        ImGui::SetCursorPosX(ImGui::GetCursorPosX() + available.x -﻿
+﻿                             button_size.x - 70);﻿
+﻿        ImGui::SetCursorPosY(ImGui::GetCursorPosY() + available.y -﻿
+﻿                             button_size.y);﻿
+﻿    1﻿
+﻿        static bool openDialog = false;﻿
+﻿﻿
+﻿ ﻿
+﻿        if (ImGui::Button("Load", button_size)) {﻿
+﻿            openDialog = true;        ﻿
+﻿            }﻿
+﻿            ﻿
+﻿        if (openDialog) {﻿
+﻿            IGFD::FileDialogConfig config;﻿
+﻿            config.path = ".";﻿
+﻿            ImGuiFileDialog::Instance()->OpenDialog(﻿
+﻿                "TxtMemoryDialog", "Escolha o arquivo de memória (.txt)",﻿
+﻿                ".txt", config);﻿
+﻿        }﻿
+﻿    // Depois que o caminho for preenchido﻿
+﻿        static std::string currentPath;﻿
+﻿        if (ImGuiFileDialog::Instance()->Display("TxtMemoryDialog")) {﻿
+﻿            if (ImGuiFileDialog::Instance()->IsOk()) {﻿
+﻿                std::string filePathName;﻿
+﻿                std::string filePath;﻿
+﻿                filePathName = ImGuiFileDialog::Instance()->GetFilePathName();﻿
+﻿                filePath = ImGuiFileDialog::Instance()->GetCurrentPath();﻿
+﻿﻿
+﻿                FILE* f = fopen(filePathName.c_str(), "r");﻿
+﻿                if (f) {﻿
+﻿                    int16_t value;﻿
+﻿                    int i = 0;﻿
+﻿                    while (fscanf(f, "%hd", &value) == 1) {﻿
+﻿                        buffer[i] = (int16_t)value;﻿
+﻿                        i++;﻿
+﻿                    }﻿
+﻿                    fclose(f);﻿
+﻿      } else {﻿
+﻿      ﻿
+﻿        std::cout << "Erro ao abrir o arquivo: " << selectedPath << "\n";﻿
+﻿        perror("Erro ao abrir o arquivo.");﻿
+﻿                }﻿
+﻿                currentPath = filePathName;﻿
+﻿            }﻿
+﻿            ImGuiFileDialog::Instance()->Close();﻿
+﻿            openDialog = false;﻿
+﻿        }﻿
+﻿        ImGui::Text("Arquivo selecionado:");﻿
+﻿        ImGui::Text("%s", currentPath.c_str());﻿
+﻿        ImGui::End();﻿
+﻿    }﻿
+﻿}﻿
+﻿
