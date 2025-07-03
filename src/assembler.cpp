a15fe14f66d3d1e755b444e25d1c8a5ad374a965
gckneip <gckneip@inf.ufpel.edu.br>
2025-07-03 16:22:45 -0300

    Primeira versão da função WriteObjectCodeFile

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -1,39 +1,54 @@
 #include "assembler.hpp"
 
+#include <fstream>
+
 Assembler::Assembler(const std::string &asmFilePath,
                      const std::string &objFilePath,
                      const std::string &lstFilePath) {
-  this->asmFilePath = asmFilePath;
-  this->objFilePath = objFilePath;
-  this->lstFilePath = lstFilePath;
-  this->locationCounter = 0;
-  this->lineCounter = 0;
+    this->asmFilePath = asmFilePath;
+    this->objFilePath = objFilePath;
+    this->lstFilePath = lstFilePath;
+    this->locationCounter = 0;
+    this->lineCounter = 0;
 }
 
 AssemblerExitCode Assembler::Assemble() {
-  this->finalExitCode = this->FirstPass();
-  if (this->finalExitCode != SUCCESS)
+    this->finalExitCode = this->FirstPass();
+    if (this->finalExitCode != SUCCESS) return finalExitCode;
+    this->finalExitCode = this->SecondPass();
     return finalExitCode;
-  this->finalExitCode = this->SecondPass();
-  return finalExitCode;
 }
 
 AssemblerExitCode Assembler::FirstPass() {
-  AssemblerExitCode exitCode = SUCCESS;
-  // faz a primeira passagem
+    AssemblerExitCode exitCode = SUCCESS;
+    // faz a primeira passagem
 
-  return exitCode;
+    return exitCode;
 }
 
 AssemblerExitCode Assembler::SecondPass() {
-  AssemblerExitCode exitCode = SUCCESS;
+    AssemblerExitCode exitCode = SUCCESS;
 
-  // faz a (adivinha?) segunda passagem
-  return exitCode;
+    // faz a (adivinha?) segunda passagem
+    return exitCode;
 }
 
 // escreve o arquivo .obj com base no vetor this->objectCode
-void Assembler::WriteObjectCodeFile() {}
+void Assembler::WriteObjectCodeFile() {
+    std::ofstream objFile(this->objFilePath, std::ios::binary);
+
+    if (!objFile) {
+        // Sei q idealmente isso nunca falha mas se falhar qual erro eu devo
+        // colocar?
+        //  this->finalExitCode = FILE_ERROR;
+        return;
+    }
+
+    if (!this->objectCode.empty()) {
+        objFile.write(reinterpret_cast<const char *>(this->objectCode.data()),
+                      this->objectCode.size() * sizeof(int16_t));
+    }
+}
 
 // escreve o arquivo .lst com this->listingLines e this->listingErrors
 void Assembler::WriteListingFile() {}
