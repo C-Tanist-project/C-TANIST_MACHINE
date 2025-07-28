01e42e627fe16d78a0a8227fb9b8159de234c426
gckneip <gckneip@inf.ufpel.edu.br>
2025-07-28 08:16:59 -0300

    Create linker skeleton and Read obj function

---
+++ src/linker.hpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+class Linker {
+ private:
+  struct Module {
+    std::string name;
+    std::vector<int16_t> code;
+    std::unordered_map<std::string, int16_t> intDefTable;
+    std::unordered_map<std::string, std::vector<int>> intUseTable;
+    int16_t stackSize = 0;
+    int16_t startAddress = 0;
+    int16_t loadAddress = 0;
+  };
+
+  std::vector<Module> modules;
+
+  std::unordered_map<std::string, std::pair<int16_t, std::string>>
+      globalSymbolTable;
+  std::vector<std::string> errors;
+  int16_t totalStackSize = 0;
+  int16_t currentLoadAddress = 0;
+
+  void FirstPass(const std::vector<std::string>& objFilePaths);
+  void SecondPass();
+  void GenerateOutput(const std::string& outputName);
+  void ReadObjectCodeFile(const std::string& filePath);
+
+ public:
+  Linker();
+  void Link(const std::vector<std::string>& objFilePaths);
+};
